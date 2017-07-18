4ab1b8dc81dd41587dae29794fae99b0e2adf2b4
wbdana <william.b.dana@gmail.com>
2017-07-17 22:39:01 -0400

    Done.

--- index.js
+++ index.js
@@ -26,22 +26,3 @@ function iLoveTheBeatles(number) {
   while (number < 15);
   return strings;
 }
-
-
-
-//
-//
-// for (i = 0; i < 5; i++) {
-//     text += "The number is " + i + "<br>";
-// }
-//
-// Statement 1 sets a variable before the loop starts (var i = 0).
-//
-// Statement 2 defines the condition for the loop to run (i must be less than 5).
-//
-// Statement 3 increases a value (i++) each time the code block in the loop has been executed.
-
-// while (i < 10) {
-//     text += "The number is " + i;
-//     i++;
-// }
